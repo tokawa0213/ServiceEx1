5a6c056098614988ab71efeb7bfbc917a3508d51
okawa.t.af <tokawa0213@gmail.com>
2018-06-06 18:15:38 +0900

    Test3

--- app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/MainActivity.java
@@ -1,20 +1,45 @@
 package jp.ac.titech.itpro.sdl.serviceex1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.content.BroadcastReceiver;
+import android.widget.Toast;
+
+//recieve from test service 3 using broadcast reciever
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
 
+    private BroadcastReceiver reciever;
+    private  IntentFilter filter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
 
+        reciever = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                Log.d(TAG, "onReceive: " + action);
+                if(action == null){ return;}
+                switch (action) {
+                    case TestService3.ACTION_ANSWER:
+                        String mes = intent.getStringExtra(TestService3.EXTRA_ANSWER);
+                        Toast.makeText(context, mes, Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        };
+        filter = new IntentFilter();
+        filter.addAction(TestService3.ACTION_ANSWER);
     }
 
     public void pressTest1(View v) {
@@ -23,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void pressTest2(View v) {
         testService2();
+    }
+
+    public void pressTest3(View v) {
+        testService3();
     }
 
     private void testService1() {
@@ -37,6 +66,25 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, TestService2.class);
         intent.putExtra(TestService2.EXTRA_MYARG, "Hello, Service2");
         startService(intent);
+    }
+
+    private void testService3() {
+        Log.d(TAG, "testService3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, TestService3.class);
+        intent.putExtra(TestService3.EXTRA_MYARG, "Hello, Service3");
+        startService(intent);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        registerReceiver(reciever,filter);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        unregisterReceiver(reciever);
     }
 
 }
---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/serviceex1/TestService3.java
@@ -0,0 +1,42 @@
+package jp.ac.titech.itpro.sdl.serviceex1;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+//send Message using broad cast
+
+public class TestService3 extends IntentService {
+    private final static String TAG = "TestService3";
+    public final static String EXTRA_MYARG = "MYARG";
+    public final static String ACTION_ANSWER = "ANS";
+    public final static String EXTRA_ANSWER = "ANS";
+
+    TestService3() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent in " + Thread.currentThread());
+        Log.d(TAG, "myarg = " + intent.getStringExtra(EXTRA_MYARG));
+
+        Intent answerIntent = new Intent();
+        answerIntent.setAction(ACTION_ANSWER);
+        answerIntent.putExtra(EXTRA_ANSWER,"Hello !");
+        sendBroadcast(answerIntent);
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate in " + Thread.currentThread());
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy in " + Thread.currentThread());
+        super.onDestroy();
+    }
+}
